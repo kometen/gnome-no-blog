fc61988193e6d6613b985ba094a26243ea7a0aa6
Claus Guttesen <kometen@gmail.com>
2024-11-23 17:04:48 +0100

    Add support for more languages

---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const languages = {
+  en: "English",
+  dk: "Dansk",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.twitter": "Twitter",
+  },
+  dk: {
+    "nav.home": "Hjem",
+    "nav.about": "Om",
+  },
+} as const;
